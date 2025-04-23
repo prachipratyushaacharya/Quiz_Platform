eb1f594659fb8962720d7fd8ac760ec5f0b31ad6
Prachi <prachipratyushaacharya@gmail.com>
2025-04-24 00:42:33 +0530

    up.1

--- src/main/java/com/quizapp/QuizApp/dao/model/Question.java
+++ src/main/java/com/quizapp/QuizApp/dao/model/Question.java
@@ -1,33 +1,77 @@
 package com.quizapp.QuizApp.dao.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "questions")
+
 public class Question {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+
     private String text;
-    private String[] options;
+
+    private String option1;
+    private String option2;
+    private String option3;
+    private String option4;
+
     private int correctOption;
+
+    public Question() {
+    }
 
     public Question(String text, String[] options, int correctOption) {
         this.text = text;
-        this.options = options;
+        this.option1 = options[0];
+        this.option2 = options[1];
+        this.option3 = options[2];
+        this.option4 = options[3];
         this.correctOption = correctOption;
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public String getText() {
         return text;
     }
 
+    public void setText(String text) {
+        this.text = text;
+    }
+
     public String[] getOptions() {
-        return options;
+        return new String[] { option1, option2, option3, option4 };
+    }
+
+    public void setOptions(String[] options) {
+        this.option1 = options[0];
+        this.option2 = options[1];
+        this.option3 = options[2];
+        this.option4 = options[3];
     }
 
     public int getCorrectOption() {
         return correctOption;
     }
 
+    public void setCorrectOption(int correctOption) {
+        this.correctOption = correctOption;
+    }
+
+    // Optional helper methods
+
     public void display() {
         System.out.println(text);
-        for (int i = 0; i < options.length; i++) {
-            System.out.println((i + 1) + ". " + options[i]);
-        }
+        System.out.println("1. " + option1);
+        System.out.println("2. " + option2);
+        System.out.println("3. " + option3);
+        System.out.println("4. " + option4);
     }
 
     public boolean isCorrect(int userChoice) {
