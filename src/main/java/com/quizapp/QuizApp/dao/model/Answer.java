5a4a90a22f138c01682a5d01b69dae3680c9e79f
Prachi <prachipratyushaacharya@gmail.com>
2025-04-24 19:48:53 +0530

    up.8

---
+++ src/main/java/com/quizapp/QuizApp/dao/model/Answer.java
@@ -0,0 +1,23 @@
+package com.quizapp.QuizApp.dao.model;
+
+public class Answer {
+    private int id;
+    private String answer;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+}
