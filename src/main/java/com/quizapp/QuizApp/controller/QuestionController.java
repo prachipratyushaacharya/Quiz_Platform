65a2b47d45744ec8f3cded3de74290117fdb43e5
Prachi <prachipratyushaacharya@gmail.com>
2025-04-24 19:49:10 +0530

    up.8

--- src/main/java/com/quizapp/QuizApp/controller/QuestionController.java
+++ src/main/java/com/quizapp/QuizApp/controller/QuestionController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import com.quizapp.QuizApp.dao.QuestionRepository;
 import com.quizapp.QuizApp.dao.model.Question;
+import com.quizapp.QuizApp.dao.model.Answer;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/questions")
 @CrossOrigin(origins = "*")
+
 public class QuestionController {
 
     @Autowired
@@ -27,4 +30,19 @@ public class QuestionController {
     public List<Question> getAllQuestions() {
         return questionRepo.findAll();
     }
+
+    @PostMapping("/submit")
+    public ResponseEntity<Integer> submitQuiz(@RequestBody List<Answer> answers) {
+        int score = 0;
+        List<Question> questions = questionRepo.findAll();
+
+        for (int i = 0; i < questions.size(); i++) {
+            if (i < answers.size()
+                    && questions.get(i).getCorrectAnswer().equalsIgnoreCase(answers.get(i).getAnswer())) {
+                score++;
+            }
+        }
+        return ResponseEntity.ok(score);
+    }
+
 }
